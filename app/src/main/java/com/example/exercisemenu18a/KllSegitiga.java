b57b8977986c3a421681f718134f26344157841f
Riyantama <riyantama2016@gmail.com>
2020-04-07 07:09:12 +0700

    commit all

---
+++ app/src/main/java/com/example/exercisemenu18a/KllSegitiga.java
@@ -0,0 +1,14 @@
+package com.example.exercisemenu18a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KllSegitiga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kll_segitiga);
+    }
+}
